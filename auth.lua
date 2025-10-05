43a11a537c253117315da1c149c9d1be0fb276cd
theboringhumane <me@iamharsh.dev>
2025-10-05 19:28:02 +0530

    Add API key validation in auth.lua to enhance request handling

--- auth.lua
+++ auth.lua
@@ -163,6 +163,12 @@ if ALLOW_BYPASS == "true" and bypass_header_value and bypass_header_value == BYP
     return ngx.exit(ngx.HTTP_OK)
 end
 
+-- check if X-Api-Key header is present
+local api_key = headers["X-Api-Key"]
+if api_key then
+    ngx.log(ngx.INFO, "🔓 Line 112 - auth.lua:main() - Api key found in headers for request: ", ngx.var.request_uri)
+    return ngx.exit(ngx.HTTP_OK)
+
 local cookies = parse_cookies(ngx.var.http_cookie)
 local token = cookies[JWT_SALT]
 
